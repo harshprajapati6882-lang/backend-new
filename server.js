8041ac3fd0119a1b1aa39eeca6de921e92d22fd6
harshprajapati6882-lang <harshprajapati6882@gmail.com>
2026-04-09 19:40:44 +0530

    Update server.js

--- server.js
+++ server.js
@@ -16,8 +16,6 @@ app.use(express.json());
 const MONGODB_URI = process.env.MONGODB_URI || 'mongodb+srv://harshprajapati6882_db_user:mbyjv1uPdKtLBz1l@devanush.tqknxqf.mongodb.net/smm-panel?retryWrites=true&w=majority';
 
 mongoose.connect(MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
   serverSelectionTimeoutMS: 30000, // 🔥 increase timeout
 })
 .then(() => {
