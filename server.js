3bdce0a4b0399279f935c88d09947691141686ee
harshprajapati6882-lang <harshprajapati6882@gmail.com>
2026-04-10 02:54:32 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const mongoose = require('mongoose');
 mongoose.set('bufferCommands', false);
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000; 
 
 app.use(cors());
 app.use(express.json());
